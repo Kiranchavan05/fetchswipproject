f71a396f54f7e034a798cdb0a7c608c3b0f1a245
kiran chavan <125879292+Kiranchavan05@users.noreply.github.com>
2023-05-10 15:44:06 +0530

    ecommerce task9 fetch movie on page load

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
-import React,{useState} from 'react';
-import './App.css';
-import AddUsers from './Components/Users/AddUsers';
-import UsersList from './Components/Users/UsersList';
+import React, { useState, useEffect, useCallback } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
- const [usersList,setUsersList]= useState([]);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error(" Something went wrong!...Retrying ");
+      }
+      const data = await response.json();
 
- const addUserHandler=(uName,uAge,cName)=>{
-  setUsersList((prevUserList)=>{
-    return [...prevUserList, {name:uName,age:uAge ,collegename:cName, id:Math.random().toString()}]
+      const transFormMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transFormMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
-  });
- };
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading....</p>;
+  }
+
   return (
     <React.Fragment>
-      <AddUsers onAddUsers={addUserHandler}/>
-      <UsersList users={usersList}/>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
